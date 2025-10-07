77f69bd4172dbb88be82abb1ef62d183d86a270c
olehponziuk <kuznicovoleh4@icloud.com>
2025-10-08 01:10:51 +0300

    Part 4

--- main_test.go
+++ main_test.go
@@ -3,13 +3,13 @@ package main
 import "testing"
 
 func TestMain(t *testing.T) {
-    if 1+1 != 2 {
-        t.Error("Math is broken!")
-    }
+	if 1+1 != 2 {
+		t.Error("Math is broken!")
+	}
 }
 
 func TestPassing(t *testing.T) {
-    if 2+2 == 4 {
-        t.Log("Math is working correctly!")
-    }
+	if 2+2 == 4 {
+		t.Log("Math is working correctly!")
+	}
 }
