8defee873aa8c3f2a8cc22465f1dc541a5b5ecda
olehponziuk <kuznicovoleh4@icloud.com>
2025-10-08 01:06:28 +0300

    Fix test to pass

--- main_test.go
+++ main_test.go
@@ -3,8 +3,13 @@ package main
 import "testing"
 
 func TestMain(t *testing.T) {
-    // Simple test that always passes
     if 1+1 != 2 {
         t.Error("Math is broken!")
+    }
+}
+
+func TestPassing(t *testing.T) {
+    if 2+2 == 4 {
+        t.Log("Math is working correctly!")
     }
 }
